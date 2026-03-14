609b2323c319983ad7ab9c0f9ddf9373c33a3b5a
bonichristian <boni.christian@studenti.bassi.edu.it>
2026-03-14 11:53:21 +0100

    web responsive

--- script.js
+++ script.js
@@ -41,3 +41,12 @@ function copyEmail(event) {
         });
 }
 
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+
+nav.classList.toggle("active");
+toggle.classList.toggle("active");
+
+});
